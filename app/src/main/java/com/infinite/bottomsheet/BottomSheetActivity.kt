51c9c6ef1c74975f33f1f894469d79f1887d3caa
infinite-Zh <zhxuwork@163.com>
2018-01-29 10:57:44 +0800

    add callback

--- app/src/main/java/com/infinite/bottomsheet/BottomSheetActivity.kt
+++ app/src/main/java/com/infinite/bottomsheet/BottomSheetActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.support.design.widget.BottomSheetBehavior
 import android.support.v4.view.ViewCompat
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
 import kotlinx.android.synthetic.main.activity_bottom_sheet.*
 
 /**
@@ -12,18 +14,36 @@ import kotlinx.android.synthetic.main.activity_bottom_sheet.*
 
 class BottomSheetActivity : AppCompatActivity() {
 
+    companion object {
+        val TAG = BottomSheetActivity.javaClass.simpleName
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bottom_sheet)
-        val behavior=BottomSheetBehavior.from(nestedScrollview)
-
+        val behavior = BottomSheetBehavior.from(nestedScrollview)
+        behavior.peekHeight = 200
         btn_open.setOnClickListener({
-            behavior.state=BottomSheetBehavior.STATE_EXPANDED
+            behavior.state = BottomSheetBehavior.STATE_EXPANDED
         })
 
         btn_close.setOnClickListener({
-            behavior.state=BottomSheetBehavior.STATE_COLLAPSED
+            behavior.state = BottomSheetBehavior.STATE_COLLAPSED
         })
 
+        behavior.setBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+            }
+
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when (newState) {
+                    BottomSheetBehavior.STATE_COLLAPSED -> Log.d(TAG,"STATE_COLLAPSED")
+                    BottomSheetBehavior.STATE_EXPANDED -> Log.d(TAG,"STATE_EXPANDED")
+                    BottomSheetBehavior.STATE_DRAGGING -> Log.d(TAG,"STATE_DRAGGING")
+                    BottomSheetBehavior.STATE_HIDDEN -> Log.d(TAG,"STATE_HIDDEN")
+                    BottomSheetBehavior.STATE_SETTLING -> Log.d(TAG,"STATE_SETTLING")
+                }
+            }
+        })
     }
 }
